6339c278a7c484a899f891d1927eb26e2f975b59
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2025-05-19 16:35:05 +0300

    Test App

--- app/main.py
+++ app/main.py
@@ -3,4 +3,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port=5000
+
+    print(f"Now listening on port {port}")
+    app.run(host='0.0.0.0', port=port)
